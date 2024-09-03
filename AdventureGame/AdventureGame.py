f475758eddc680ee686697eb89aab573b1c07b40
JOSH-PC <User@DESKTOP-AGJD7U8>
2024-09-03 22:50:50 +1000

    Camera Follow and Test Map

--- AdventureGame/AdventureGame.py
+++ AdventureGame/AdventureGame.py
@@ -1,14 +1,17 @@
 import pygame
+import pygame.image
 
 pygame.init()
-screen_width = 512
-screen_height = 512
+screen_width = 1920
+screen_height = 1280
 screen = pygame.display.set_mode([screen_width, screen_height])
 screen.fill((0, 0, 0))
 
+background = pygame.image.load("MapForest.png").convert()
+
 class PlayerSprite(pygame.sprite.Sprite):
     def __init__(self):
-        super(pygame.sprite.Sprite, self).__init__()
+        pygame.sprite.Sprite.__init__(self)
         self.surf = pygame.Surface((25, 75))
         self.surf.fill((255, 255, 255))
         self.rect = self.surf.get_rect()
@@ -32,11 +35,32 @@ class Player(PlayerSprite):
     def __init__(self):
         super().__init__()
 
+class Camera(pygame.sprite.Group):
+    def __init__(self):
+        super().__init__()
+        self.offset = pygame.math.Vector2()
+        self.floor_rect = background.get_rect(topleft = (-1000,-600))
+
+    def custom_blit(self):
+        self.offset.x = player.rect.centerx - screen_width // 2
+        self.offset.y = player.rect.centery - screen_height // 2
+
+        background_offset_pos = self.floor_rect.topleft - self.offset
+
+        screen.blit(background, background_offset_pos)
+
+        for sprite in all_sprites_group:
+            offset_pos = sprite.rect.topleft - self.offset
+            screen.blit(sprite.surf, offset_pos)
 
 
 player = Player()
 clock = pygame.time.Clock()
 running = True
+camera = Camera()
+all_sprites_group = pygame.sprite.Group()
+
+all_sprites_group.add(player)
 
 while running:
     clock.tick(60)
@@ -44,12 +68,17 @@ while running:
     pressed_keys = pygame.key.get_pressed()
    
     for event in pygame.event.get():
+        if pressed_keys[pygame.K_ESCAPE]:
+            running = False
+
         if event.type == pygame.QUIT:
-            done = True
+            running = False
         
     player.update(pressed_keys)
     
-    screen.blit(player.surf, player.rect)
+    camera.custom_blit()
     pygame.display.flip()
     screen.fill((50, 157, 168))
-    
+   
+   
+pygame.quit()
